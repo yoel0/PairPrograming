0507c5531babe56dfe728ac56c759ec44a4cfdac
yoel <67129044+yoel0@users.noreply.github.com>
2020-10-29 23:29:28 -0700

    [add] v2 to mintimevisitingallpoints hs

--- ChallengeProblems/MinTimeVisitingAllPoints/MinTimeVisitingAllPoints.hs
+++ ChallengeProblems/MinTimeVisitingAllPoints/MinTimeVisitingAllPoints.hs
@@ -4,3 +4,19 @@ dstnc crdA crdB =  max(abs(head crdB - head crdA)) (abs(last crdB - last crdA))
 clairvoyance [] = [[0]]
 clairvoyance [x] = [[0]]
 clairvoyance (x:y:ls) = [(dstnc x y)] : clairvoyance (y:ls)
+
+-- Solution Fix
+grtst a b = max (abs (x2 - x1))(abs (y2 - y1))
+  where x1 = head a
+        x2 = head b
+        y1 = last a
+        y2 = last b
+lstTime coords = sum $ concat (f coords)
+f :: (Num a, Ord a) => [[a]] -> [[a]]
+f (x:y:xs) = [(grtst x y)] : f (y:xs)
+f (x:xs) = []
+
+-- I forgot about this idea, but here we can pattern match, 
+-- in this case (x:xs) means x is the last element in the list, 
+-- therefore we we recurse with an empty list.
+-- this means we only need one base case, which can simply be f [] = []
