ec7a0a2873c5cc6964b76edace7f5721e34fc179
yoel <67129044+yoel0@users.noreply.github.com>
2020-10-30 20:06:57 -0700

    [add] formatDuration

---
+++ Tests/formatDuration.js
@@ -0,0 +1,19 @@
+const formatDuration = ms => {
+  if (ms < 0) ms = -ms;
+  const time = {
+    day: Math.floor(ms / 86400000),
+    hour: Math.floor(ms / 3600000) % 24,
+    minute: Math.floor(ms / 60000) % 60,
+    second: Math.floor(ms / 1000) % 60,
+    millisecond: Math.floor(ms) % 1000
+  };
+  return Object.entries(time)
+    .filter(val => val[1] !== 0)
+    .map(([key, val]) => `${val} ${key}${val !== 1 ? 's' : ''}`)
+    .join(', ');
+};
+
+console.log(formatDuration(2008)); 
+// '2 second, 8 millisecond'
+console.log(formatDuration(36325055574));
+// '420 days, 10 hours, 17 minutes, 35 seconds, 574 milliseconds'
